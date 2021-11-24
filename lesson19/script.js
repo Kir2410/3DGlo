869a9b954264b6de81ab00d914fe9a80ec8b064f
Кирилл Коршунов <kkv2410@gmail.com>
2021-11-24 16:21:41 +0700

    Feature(lesson19): create lesson19 folder

---
+++ lesson19/script.js
@@ -0,0 +1,50 @@
+const date = new Date();
+const dateNewYear = new Date('01 January 2022').getTime();
+const dateNow = new Date().getTime();
+const day = date.getDay();
+
+const week = [
+    'Воскресенье',
+    'Понедельник',
+    'Вторник',
+    'Среда',
+    'Четверг',
+    'Пятница',
+    'Суббота'
+];
+
+const getDays = () => {
+    let timeRemainig = (dateNewYear - dateNow) / 1000;
+    let days = Math.floor(((timeRemainig / 60) / 60) / 24);
+    return days;
+}
+const getTimeOfDay = () => {
+    let timeOfDay = '';
+    if (date.getHours() >= 0 && date.getHours() <= 5) {
+        timeOfDay = 'Доброй ночи!';
+    } else {
+        if (date.getHours() >= 6 && date.getHours() <= 11) {
+            timeOfDay = 'Доброе утро!';
+        } else {
+            if (date.getHours() >= 12 && date.getHours() <= 17) {
+                timeOfDay = 'Добрый день!';
+            } else {
+                if (date.getHours() >= 18 && date.getHours() <= 23) {
+                    timeOfDay = 'Добрый вечер!';
+                }
+            }
+        }
+    }
+    return timeOfDay;
+}
+
+
+console.log(`${getTimeOfDay()}
+Сегодня: ${week[day]}
+Текущее время: ${date.toLocaleTimeString('en')}
+До нового года осталось ${getDays()} дней`);
+
+// Вечер: с 0:00 до 5:29
+// Утро: с 5:30 до 11:59
+// После полудня: с 12:00 до 17:59
+// Вечер: с 18:00 до 24:00
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import timer from './modules/timer'
 
-timer('24 november 2021 16:12')
+timer('24 november 2021 15:46')
