050d3662800c7f33888084f1cba6ca8f0f7b303e
Кирилл Коршунов <kkv2410@gmail.com>
2021-11-25 22:46:07 +0700

    Feature (modal.js): add animation for modal

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,17 +1,32 @@
 const modal = () => {
     const modal = document.querySelector('.popup');
+    const modalContent = document.querySelector('.popup-content');
     const buttons = document.querySelectorAll('.popup-btn');
     const closeBtn = modal.querySelector('.popup-close');
+    let count = -50;
+
+    const animationModal = () => {
+        let animStart = requestAnimationFrame(animModal);
+        modal.style.display = 'block';
+        if (count < 21) {
+            modalContent.style.top = `${count}%`;
+        } else {
+            cancelAnimationFrame(animStart);
+            count = -50;
+        }
+        count += 2;
+    };
 
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            modal.style.display = 'block';
+            animationModal();
+            console.dir(modal);
         })
-    })
+    });
 
     closeBtn.addEventListener('click', () => {
         modal.style.display = '';
-    })
+    });
 };
 
 export default modal;
