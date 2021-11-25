093dda2165623acc2fb1702277b26ed7112eb832
Кирилл Коршунов <kkv2410@gmail.com>
2021-11-25 17:12:23 +0700

    Feature (modal.js): create function to show modal on display

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
+import modal from './modules/modal'
 
 timer('25 november 2021 17:30')
 menu()
+modal()
---
+++ src/modules/modal.js
@@ -0,0 +1,17 @@
+const modal = () => {
+    const modal = document.querySelector('.popup');
+    const buttons = document.querySelectorAll('.popup-btn');
+    const closeBtn = modal.querySelector('.popup-close');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            modal.style.display = 'block';
+        })
+    })
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = '';
+    })
+};
+
+export default modal;
