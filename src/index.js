f32741afeba6f642e3dd9c097b586b423065601e
Кирилл Коршунов <kkv2410@gmail.com>
2021-11-28 23:35:53 +0700

    Feature (validator.js): create validator for input forms

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
+import validator from './modules/validator'
 
 timer('25 november 2021 23:11')
 menu()
 modal()
+validator()
---
+++ src/modules/validator.js
@@ -0,0 +1,38 @@
+const validator = () => {
+    const calculate = document.querySelector('.calc-block');
+    const calcItems = calculate.querySelectorAll('input.calc-item');
+    const inputText = document.querySelectorAll('input[placeholder="Ваше имя"]');
+    const inputMessage = document.querySelector('input[placeholder="Ваше сообщение"]');
+    const inputMail = document.querySelectorAll('input[type=email]');
+    const inputTel = document.querySelectorAll('input[type=tel]');
+
+    calcItems.forEach(item => {
+        item.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^0-9]+/, "");
+        })
+    });
+
+    inputText.forEach(item => {
+        item.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^А-Яа-я\s\-]+/, "");
+        })
+    });
+
+    inputMessage.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/[^А-Яа-я\s\-]+/, "");
+    });
+
+    inputMail.forEach(item => {
+        item.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^A-Za-z\@\-\_\.\!\~\*\']+/, "");
+        })
+    });
+
+    inputTel.forEach(item => {
+        item.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^0-9\(\)\-]+/, "");
+        })
+    });
+}
+
+export default validator;
