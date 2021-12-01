b353521791585b44fb0a87d518b79dacd6c6b01a
Кирилл Коршунов <kkv2410@gmail.com>
2021-12-02 00:19:27 +0700

    Fix (menu.js): fixed addEventListener's arguments

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -6,8 +6,8 @@ const menu = () => {
     };
 
     document.addEventListener('click', (e) => {
-        if (e.target.closest('.menu') || e.target.closest('li') || e.target.classList.contains('close-btn') || (!e.target.classList.contains('active-menu') && menu.classList.contains('active-menu'))) {
-            console.log('мимо')
+        if (e.target.closest('.menu') || (e.target.tagName === 'A' && menu.classList.contains('active-menu')) || e.target.classList.contains('close-btn') || (!e.target.classList.contains('active-menu') && menu.classList.contains('active-menu') && !e.target.closest('li'))) {
+            console.dir(e.target)
             handleMenu();
         }
     })
