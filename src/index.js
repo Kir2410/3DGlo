bed86a12098f07b4b7a23a37b1859aa1a76d0fa1
Кирилл Коршунов <kkv2410@gmail.com>
2021-12-02 23:53:05 +0700

    Feature (src): create slider.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from './modules/menu'
 import modal from './modules/modal'
 import validator from './modules/validator'
 import tabs from './modules/tabs'
+import slider from './modules/slider'
 
 timer('25 november 2021 23:11')
 menu()
 modal()
 validator()
 tabs()
+slider()
