a17f9b6269b803bc64c4c77e1244fa59050db8ba
Кирилл Коршунов <kkv2410@gmail.com>
2021-12-03 10:42:33 +0700

    Feature (slider.js): create addDot function

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,11 +1,22 @@
 const slider = () => {
     const sliderBlock = document.querySelector('.portfolio-content')
     const slides = document.querySelectorAll('.portfolio-item')
-    const dots = document.querySelectorAll('.dot')
     const timerInterval = 2000
 
+    let dots = document.querySelectorAll('.dot')
     let currentSlide = 0
     let interval
+
+    const addDot = () => {
+        let count = slides.length - 1
+        while (count > 0) {
+            const cloneDot = dots[0].cloneNode(true);
+            dots[dots.length - 1].after(cloneDot);
+            dots[dots.length - 1].classList.remove('dot-active')
+            dots = document.querySelectorAll('.dot')
+            count--
+        }
+    }
 
     const prevSlide = (elems, index, strClass) => {
         elems[index].classList.remove(strClass)
@@ -13,8 +24,8 @@ const slider = () => {
     const nextSlide = (elems, index, strClass) => {
         elems[index].classList.add(strClass)
     }
-
     const autoSlide = () => {
+
         prevSlide(slides, currentSlide, 'portfolio-item-active')
         prevSlide(dots, currentSlide, 'dot-active')
         currentSlide++
@@ -24,11 +35,9 @@ const slider = () => {
         nextSlide(slides, currentSlide, 'portfolio-item-active')
         nextSlide(dots, currentSlide, 'dot-active')
     }
-
     const startSlide = (timer = 1500) => {
         interval = setInterval(autoSlide, timer)
     }
-
     const stopSlide = () => {
         clearInterval(interval)
     }
@@ -65,20 +74,20 @@ const slider = () => {
         nextSlide(slides, currentSlide, 'portfolio-item-active')
         nextSlide(dots, currentSlide, 'dot-active')
     })
-
     sliderBlock.addEventListener('mouseenter', (e) => {
         if (e.target.matches('.dot, .portfolio-btn')) {
             stopSlide()
         }
     }, true)
-
     sliderBlock.addEventListener('mouseleave', (e) => {
         if (e.target.matches('.dot, .portfolio-btn')) {
             startSlide(timerInterval)
         }
     }, true)
 
+    addDot()
     startSlide(timerInterval)
+    console.dir(dots)
 }
 
 export default slider
