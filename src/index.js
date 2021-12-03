a6cb536755d14b5b5e04dd56854336da0d65552d
Кирилл Коршунов <kkv2410@gmail.com>
2021-12-03 15:52:37 +0700

    Feature(calc.js): create calc function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal'
 import validator from './modules/validator'
 import tabs from './modules/tabs'
 import slider from './modules/slider'
+import calc from './modules/calc'
 
 timer('25 november 2021 23:11')
 menu()
@@ -11,3 +12,4 @@ modal()
 validator()
 tabs()
 slider()
+calc(100)
