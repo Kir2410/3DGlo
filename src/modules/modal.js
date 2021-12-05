1847c3f890e345aad277e41552c51d97d50849c7
Кирилл Коршунов <kkv2410@gmail.com>
2021-12-05 13:23:07 +0700

    Feature (src): create helpers.js and use function animate from helpers.js in modal.js

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,26 +1,26 @@
+import {
+    animate
+} from "./helpers"
+
 const modal = () => {
     const modal = document.querySelector('.popup');
     const modalContent = document.querySelector('.popup-content');
     const buttons = document.querySelectorAll('.popup-btn');
-    const closeBtn = modal.querySelector('.popup-close');
-    let count = -50;
-
-    const animationModal = () => {
-        let animStart = requestAnimationFrame(animationModal);
-        modal.style.display = 'block';
-
-        if (count < 21) {
-            modalContent.style.top = `${count}%`;
-        } else {
-            cancelAnimationFrame(animStart);
-            count = -50;
-        }
-        count += 2;
-    };
 
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            window.innerWidth >= 768 ? animationModal() : modal.style.display = 'block';
+            modal.style.display = 'block';
+            if (window.innerWidth >= 768) {
+                animate({
+                    duration: 500,
+                    timing(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw(progress) {
+                        modalContent.style.opacity = progress;
+                    }
+                })
+            }
         })
     });
 
