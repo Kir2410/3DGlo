17ed14c13605cb9d82e24ced0a4c9782a2cd8db6
Кирилл Коршунов <kkv2410@gmail.com>
2021-12-05 13:30:19 +0700

    Style (menu.js): delete console.dir()

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -7,7 +7,6 @@ const menu = () => {
 
     document.addEventListener('click', (e) => {
         if (e.target.closest('.menu') || (e.target.tagName === 'A' && menu.classList.contains('active-menu')) || e.target.classList.contains('close-btn') || (!e.target.classList.contains('active-menu') && menu.classList.contains('active-menu') && !e.target.closest('li'))) {
-            console.dir(e.target)
             handleMenu();
         }
     })
