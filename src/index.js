c5b5f42ca007d3ebcaf5d8e0479e18a98f732619
Кирилл Коршунов <kkv2410@gmail.com>
2021-12-13 10:20:52 +0700

    Feature: connect sendForm to modal and footer-form

--- src/index.js
+++ src/index.js
@@ -5,11 +5,29 @@ import validator from './modules/validator'
 import tabs from './modules/tabs'
 import slider from './modules/slider'
 import calc from './modules/calc'
+import sendForm from './modules/sendForm'
 
-timer('25 november 2021 23:11')
+timer('31 december 2021 23:59')
 menu()
 modal()
 validator()
 tabs()
 slider()
 calc(100)
+sendForm({
+    formId: 'form1',
+    someElem: [{
+        type: 'block',
+        id: 'total'
+    }]
+})
+sendForm({
+    formId: 'form2',
+    someElem: [{
+        type: 'input',
+        id: 'form2-message'
+    }]
+})
+sendForm({
+    formId: 'form3'
+})
